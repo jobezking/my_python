1b527a8b5a111d4b019d613a4645f20d25e99bac
jobezking <52982865+jobezking@users.noreply.github.com>
2025-06-19 19:07:52 -0400

    oop.py

---
+++ exercises/oop.py
@@ -0,0 +1,35 @@
+#Creating an instance of a class
+#When writing a Python class, you define a method called __init__ to be your constructor.
+# prints "red"
+class Apple:
+    def __init__(self):
+        self.color = "red"
+        self.flavor = "sweet"
+
+honeycrisp = Apple()
+print(honeycrisp.color)
+
+#Modify variables
+# prints "sweet" and "tart"
+class Apple:
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+
+honeycrisp = Apple("red", "sweet")
+fuji = Apple("red", "tart")
+print(honeycrisp.flavor)
+print(fuji.flavor)
+
+#special methods
+# prints "an apple which is red and sweet"
+class Apple:
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+
+    def __str__(self):
+        return "an apple which is {} and {}".format(self.color, self.flavor)
+
+honeycrisp = Apple("red", "sweet")
+print(honeycrisp)
