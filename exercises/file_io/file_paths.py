8816ee6720426e98e487581526c96f3c67292615
jobezking <52982865+jobezking@users.noreply.github.com>
2025-06-23 01:53:52 -0400

    Update file_paths.py

--- exercises/file_io/file_paths.py
+++ exercises/file_io/file_paths.py
@@ -7,7 +7,7 @@ C:/my-directory/target-file.txt
 #Windows file directory
 C:\\my-directory\\target-file.txt
 
-#CWD command: 
+#CWD command returns the current working directory for use in relative paths: 
 os.getcwd()
 
 #CWD command for external files:
