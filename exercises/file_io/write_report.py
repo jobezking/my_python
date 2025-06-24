d5fd51a6d28680752c2117ca06ad0be01904cd2b
jobezking <52982865+jobezking@users.noreply.github.com>
2025-06-23 23:05:39 -0400

    update

--- exercises/file_io/write_report.py
+++ exercises/file_io/write_report.py
@@ -26,3 +26,27 @@ def write_report(dictionary, report_file):
         for k in sorted(dictionary):
             f.write(str(k)+':'+str(dictionary[k])+'\n')
         f.close()
+
+
+'''
+Terms and definitions from course 2, module 2
+Absolute path: A full path to the resource in the file system
+
+Comma separated values (CSV): A very common data format used to store data as segment of text separated by commas
+
+Dialects: Rules that define how a CSV file is structured
+
+File systems: Methods and structures used to organize and control how data is stored and accessed
+
+Mode: The format controlling what you can do with a recently opened file
+
+Qwiklabs: An online learning environment or virtual machine to simulate real-world scenarios
+
+Reader objects:  Object that represents an element or entity within a scene that needs to be rendered to the screen
+
+Relative path: A portion of a path to show where the resource is located in relation to the current working directory
+
+Virtual machine (VM): A computer simulated through software
+
+Writer objects: The capability to write data to a CSV file
+'''
