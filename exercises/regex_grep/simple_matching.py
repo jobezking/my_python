c3131652296e94683624bdb97af3c1d145645438
jobezking <52982865+jobezking@users.noreply.github.com>
2025-06-24 15:46:07 -0400

    repetition

--- exercises/regex_grep/simple_matching.py
+++ exercises/regex_grep/simple_matching.py
@@ -15,3 +15,40 @@ print(re.search(r"p.ng", "clapping"))
 print(re.search(r"p.ng", "sponge"))
 
 print(re.search(r"p.ng", "Pangaea", re.IGNORECASE))
+
+print(re.search(r"[Pp]ython", "Python"))
+
+print(re.search(r"[a-z]way", "The end of the highway"))
+print(re.search(r"[a-z]way", "What a way to go"))
+print(re.search("cloud[a-zA-Z0-9]", "cloudy"))
+print(re.search("cloud[a-zA-Z0-9]", "cloud9"))
+
+print(re.search(r"[^a-zA-Z]", "This is a sentence with spaces."))
+print(re.search(r"[^a-zA-Z ]", "This is a sentence with spaces."))
+
+print(re.search(r"cat|dog", "I like cats."))
+print(re.search(r"cat|dog", "I love dogs!"))
+print(re.search(r"cat|dog", "I like both dogs and cats."))
+
+print(re.search(r"cat|dog", "I like cats."))
+print(re.search(r"cat|dog", "I love dogs!"))
+print(re.search(r"cat|dog", "I like both dogs and cats."))
+print(re.findall(r"cat|dog", "I like both dogs and cats."))
+
+def check_aei (text):
+  result = re.search(r"a.e.i", text)
+  return result != None
+
+def check_punctuation (text):
+  result = re.search(r"[,|.|:|;|?|!]", text)
+  return result != None
+
+print(check_aei("academia")) # True
+print(check_aei("aerial")) # False
+print(check_aei("paramedic")) # True
+
+print(check_punctuation("This is a sentence that ends with a period.")) # True
+print(check_punctuation("This is a sentence fragment without a period")) # False
+print(check_punctuation("Aren't regular expressions awesome?")) # True
+print(check_punctuation("Wow! We're really picking up some steam now!")) # True
+print(check_punctuation("End of the line")) # False
